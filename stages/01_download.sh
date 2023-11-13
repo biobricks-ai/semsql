146a99fe65efa27ea84e2d81467c14d50a241a3e
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-11-13 10:18:06 -0500

    Additionally download via obo.tsv list

--- stages/01_download.sh
+++ stages/01_download.sh
@@ -11,20 +11,31 @@ brickpath="$localpath/brick"
 mkdir -p $brickpath
 echo "Brick path: $brickpath"
 
-# Define the YAML list URL
-list_url="https://raw.githubusercontent.com/INCATools/semantic-sql/main/src/semsql/builder/registry/ontologies.yaml"
+# Define the ontologies.yaml list URL
+onto_url="https://raw.githubusercontent.com/INCATools/semantic-sql/main/src/semsql/builder/registry/ontologies.yaml"
+# Define the obo.tsv list URL
+obo_url="https://raw.githubusercontent.com/INCATools/semantic-sql/main/reports/obo.tsv"
 
 # Retrieve and unpack the files
+
+# Via ontologies.yaml:
+#
 # NOTE using CPAN::Meta::YAML because nothing needs to be installed (no need
 # for YAML::Tiny, YAML::PP, etc.). It parses fine even if it is meant to be
 # a limited feature set of YAML.
-curl -L $list_url | tee $brickpath/ontologies.yaml | perl -MCPAN::Meta::YAML -0777 -e '
+curl -L $onto_url | tee $brickpath/ontologies.yaml | perl -MCPAN::Meta::YAML -0777 -e '
 my $text  = <>;
 my $ontos = CPAN::Meta::YAML->read_string($text)
           or die CPAN::Meta::YAML->errstr;
 for my $onto (keys %{ $ontos->[0]{ontologies} }) {
 	print "https://s3.amazonaws.com/bbop-sqlite/${onto}.db.gz", "\n";
 }
+' | xargs -n1 -P$(nproc) wget -P $brickpath
+
+# Via obo.tsv:
+curl -L $obo_url | tee $brickpath/obo.tsv | perl -lpe '
+        my $onto = $_;
+        $_ = qq{https://s3.amazonaws.com/bbop-sqlite/${onto}.db.gz}
 ' | xargs -n1 -P$(nproc) wget -P $brickpath
 
 echo "Download done."
