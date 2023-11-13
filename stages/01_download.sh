2039adb95ff92bcdf14d9349c24bff85d2d93a15
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-11-13 10:30:30 -0500

    Combine and uniq the list of downloads

--- stages/01_download.sh
+++ stages/01_download.sh
@@ -18,24 +18,29 @@ obo_url="https://raw.githubusercontent.com/INCATools/semantic-sql/main/reports/o
 
 # Retrieve and unpack the files
 
-# Via ontologies.yaml:
-#
-# NOTE using CPAN::Meta::YAML because nothing needs to be installed (no need
-# for YAML::Tiny, YAML::PP, etc.). It parses fine even if it is meant to be
-# a limited feature set of YAML.
-curl -L $onto_url | tee $brickpath/ontologies.yaml | perl -MCPAN::Meta::YAML -0777 -e '
-my $text  = <>;
-my $ontos = CPAN::Meta::YAML->read_string($text)
-          or die CPAN::Meta::YAML->errstr;
-for my $onto (keys %{ $ontos->[0]{ontologies} }) {
-	print "https://s3.amazonaws.com/bbop-sqlite/${onto}.db.gz", "\n";
-}
-' | xargs -n1 -P$(nproc) wget -P $brickpath
-
-# Via obo.tsv:
-curl -L $obo_url | tee $brickpath/obo.tsv | perl -lpe '
-        my $onto = $_;
-        $_ = qq{https://s3.amazonaws.com/bbop-sqlite/${onto}.db.gz}
-' | xargs -n1 -P$(nproc) wget -P $brickpath
+cat \
+        <(
+                # Via ontologies.yaml:
+                #
+                # NOTE using CPAN::Meta::YAML because nothing needs to be installed (no need
+                # for YAML::Tiny, YAML::PP, etc.). It parses fine even if it is meant to be
+                # a limited feature set of YAML.
+                curl -L $onto_url | tee $brickpath/ontologies.yaml | perl -MCPAN::Meta::YAML -0777 -e '
+                my $text  = <>;
+                my $ontos = CPAN::Meta::YAML->read_string($text)
+                          or die CPAN::Meta::YAML->errstr;
+                for my $onto (keys %{ $ontos->[0]{ontologies} }) {
+                        print "https://s3.amazonaws.com/bbop-sqlite/${onto}.db.gz", "\n";
+                }
+                '
+        ) \
+        <(
+                # Via obo.tsv:
+                curl -L $obo_url | tee $brickpath/obo.tsv | perl -lpe '
+                        my $onto = $_;
+                        $_ = qq{https://s3.amazonaws.com/bbop-sqlite/${onto}.db.gz}
+                '
+        ) \
+        | sort -u | xargs -n1 -P$(nproc) wget -P $brickpath
 
 echo "Download done."
